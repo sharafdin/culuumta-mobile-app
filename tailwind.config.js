2e2bfed8938a096a2afc896d72d4e5899e514a0c
Mr Sharafdin <isasharafdin@gmail.com>
2023-10-16 14:51:45 +0300

    changed to flutter

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
